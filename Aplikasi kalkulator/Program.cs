8e4b9ea00ea8ad0ad918e867eacd79e3fb6271aa
emailfattaahherdansyah@students.amikom.ac.id <arif.kurniawan@students.amikom.ac.id>
2023-03-14 14:53:35 +0700

    commit ke-1

---
+++ Aplikasi kalkulator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikasi_kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ini apps sederhana menggunakan ";
+            Console.Write("Operasi apa yang ingin diterjemahkan? \n 1. '+' \n 2. '-' \n 3. 'x' \n 4. '/' \n :");
+            char Operasi = char.Parse(Console.ReadLine());
+
+            switch(Operasi)
+            {
+                case "+":
+
+            }
+
+        }
+    }
+}
